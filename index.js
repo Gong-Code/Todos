3db481073bed05ab6862407d1a6a5571eb9c5e92
Gong Moonphruk <gong.moonphruk@gmail.com>
2023-11-15 15:29:16 +0100

    fixed white spaces

--- index.js
+++ index.js
@@ -109,30 +109,25 @@ todoList.addEventListener('click', deleteTodo);
 
 // Functions
 function createTodo() {
-    //Todo Div (Parent)
     const todoDiv = document.createElement('div');
     todoDiv.classList.add('todo');
 
-    //Create Li
     const newTodo = document.createElement('li');
     newTodo.innerText = todoInput.value;
     newTodo.classList.add('todo-item');
     newTodo.setAttribute("id", "list-item")
     todoDiv.appendChild(newTodo);
     
-    //Check Mark Button
     const completedButton = document.createElement('button');
     completedButton.innerHTML = '<i class="fas fa-check"></i>';
     completedButton.classList.add('complete-btn');
     todoDiv.appendChild(completedButton);
 
-    //Check Trash Button
     const trashButton = document.createElement('button');
     trashButton.innerHTML = '<i class="fas fa-trash"></i>';
     trashButton.classList.add('trash-btn');
     todoDiv.appendChild(trashButton);
 
-    //Append to List
     todoList.appendChild(todoDiv);
 }
 
